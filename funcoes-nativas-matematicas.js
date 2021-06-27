ca8004a19aeaf78fefde181dec1bb369c7a31605
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-27 17:44:01 -0300

    Commit da aula 196 sobre funções natívas-Matemáticas. Nessa aula foram demonstradas diversas funções da função nativa Math e onde conseguir informação sobre todas as opçoes que essa função builtin provem.

---
+++ funcoes-nativas-matematicas.js
@@ -0,0 +1,13 @@
+let numero = 280.5
+//Math.PI
+//Math.ceil(numero) Arredonda pra cima
+//Math.floor(numero) Arredonda pra baixo
+
+//Math.round(numero) Arredonda
+
+//Math.random() * 100 // entre 0 e 1
+//Math.min(15,10)
+//Math.max(15,10)
+
+//Math.pow(9,2)
+//Math.sqrt(81)
