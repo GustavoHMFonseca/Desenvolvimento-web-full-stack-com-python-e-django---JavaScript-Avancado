1740ffb1280458d461b39df62d433d20b26cc000
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-27 19:46:06 -0300

    Commit da aula 202 sobre array:filter. Nessa aula foi ensinado sobre a função filter que basicamente retorna um array filtrados.

---
+++ array-filter.js
@@ -0,0 +1,23 @@
+/*
+const usuario = [
+  {nome: "João", idade: 32},
+  {nome: "Ana", idade: 16},
+  {nome: "Maria", idade: 40}
+]
+const funcao = function(item, i,arr){
+  return item.idade > this.filtro
+}
+const filtro = {
+  filtro:18
+}
+const usuariosMaiorIdade = usuario.filter(funcao,filtro)
+console.log(usuariosMaiorIdade)
+
+*/
+
+const carros = [
+  {nome:"Gol", marca: "Volkswagem"},
+  {nome:"iX35", marca: "hyundai"},
+  {nome:"Santa Fé", marca: "hyundai"},
+  {nome:"polo", marca: "Volkswagem"},
+]
