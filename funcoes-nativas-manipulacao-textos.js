4b96c4069fb55e26ece00dde0480ad42424a4902
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-27 17:34:00 -0300

    Commit da aula 195 sobre funções nativas - manipulação de texto. Nessa aula é demonstrada diversas funções builtins e atributos de variável texto paraa manipulação e coleta de dados sobre textos.

---
+++ funcoes-nativas-manipulacao-textos.js
@@ -0,0 +1,53 @@
+/*
+  const nome ={
+  texto: "Gustavo",
+  legth: 7
+  charAt(){}
+  }
+*/
+
+//"teste".length
+
+/*
+let nome = "carro do Gustavo"
+nome.length
+nome.charAt(15)
+let n = nome.replace("Gustavo", "joão")
+console.log(n)
+*/
+/*
+let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+frase.substr(0,15)
+*/
+/*
+let nome = "Gustavo Henrique"
+let resultado = nome.split(" ")
+console.log(resultado)
+*/
+/*
+let nome = "José"
+let sobrenome = "da Silva"
+let s = sobrenome.slice(0,5)
+nome + s + "@empresa.com.br"
+*/
+/*
+let nome = "Pedro Silva"
+nome.toUpperCase()
+nome.toLowerCase()
+*/
+/*
+let nome = "    Pedro Silva            "
+nome
+nome.trim()
+*/
+
+let a = "olá"
+let b = "Gustavo"
+let c = "Henrique"
+
+a.concat(" ",b," ",c)
+
+
+
+
+
