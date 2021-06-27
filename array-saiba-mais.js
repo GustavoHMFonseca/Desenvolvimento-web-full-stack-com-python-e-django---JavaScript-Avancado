8359405520fc8577257af251d45818c8ce79f5d2
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-27 18:12:02 -0300

    Commit sobre a aula 198 sobre arrays. Nessa aula é aprofundado o conhecimento de arrays, demonstrando formas de escrever, propriedades e funções de arrays na prática.

---
+++ array-saiba-mais.js
@@ -0,0 +1,22 @@
+/*
+const lista = []
+
+const lista2 = new Array("Itens...")
+typeof(lista)
+typeof new Array
+*/
+/*
+const nomes = ["João","Maria"]
+const frutas = new Array("Morango", "Banana")
+
+console.log(nomes[0])
+console.log(frutas[1])
+*/
+
+const nomes = ["João", "Maria", "Ana"]
+const numeros = [3,1,2]
+//nomes.push("Ana")
+//delete nomes[2]
+//nomes[0] = "Teste"
+console.log(nomes.sort())
+console.log(numeros.sort())
